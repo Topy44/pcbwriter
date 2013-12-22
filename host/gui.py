09da3027c4b0c1e2687261d6d1ed45c913f2535c
Thorin Hopkins <topy@untergrund.net>
2013-12-22 09:37:55 +0100

    Basic GUI layout added.

--- host/gui.py
+++ host/gui.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys, StringIO
 from gi.repository import Gtk
 from pcbwriter import PCBWriter
 
@@ -8,16 +9,14 @@ class Handler:
     def onDeleteWindow(self, *args):
         Gtk.main_quit(*args)
 
-    def onHomePressed(self, button):
-        pcb.home_stepper(wait=True);
+    def onImagemenuitemQuitActivate(self, *args):
+        Gtk.main_quit(*args)
 
 builder = Gtk.Builder()
-builder.add_from_file("example.glade")
+builder.add_from_file("pcbwriter.glade")
 builder.connect_signals(Handler())
 
-window = builder.get_object("window1")
+window = builder.get_object("pcbwriter")
 window.show_all()
 
 Gtk.main()
-
-print("yay!")
