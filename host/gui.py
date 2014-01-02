d35463d0b06693a356d58730288c2c947e8421b6
Thorin Hopkins <topy@untergrund.net>
2014-01-02 03:15:19 +0100

    First steps towards loading and viewing pdf files

--- host/gui.py
+++ host/gui.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
-import sys, math, cairo
-from gi.repository import Gtk
+import os, sys, math, cairo, tempfile, ghostscript, shutil
+from gi.repository import Gtk, Gdk, GdkPixbuf
 from pcbwriter import PCBWriter
 
 pcb = PCBWriter(called_from_gui=True)
 
+#print dir(GdkPixbuf.Pixbuf)
+
 # Signal handler class
 class Handler:
+    img = None
+
     def onDeleteEvent(self, *args):
         Gtk.main_quit(*args)
 
@@ -39,6 +43,10 @@ class Handler:
         cr.rectangle(da.get_allocation().width - 100, 0, 100, da.get_allocation().height)
         cr.fill()
 
+        if self.img is not None:
+            self.img.draw(cr)
+
+        # Apply scaling to fit surface
         scale = float(da.get_allocation().width) / float(180)
         cr.scale(scale, scale)
 
@@ -52,7 +60,49 @@ class Handler:
         draw_ruler(da, cr, xpos=0, ypos=20/scale, len=da.get_allocation().height / scale, scale=scale, vertical=True, skipzero=True)
 
     def on_buttonLoadimage_clicked(self, button):
+        self.img = image()
+        self.img.load("helloworld.pdf")
+
+class image:
+    def __init__(self):
+        self.imgtmp = None
+        self.pdftmp = None
         pass
+
+    def __del__(self):
+        print_to_console("\nRemoving temp files...")
+        if self.imgtmp is not None: os.remove(self.imgtmp)
+        if self.pdftmp is not None: os.remove(self.pdftmp)
+
+    def load(self, filename):
+        # Check if file exists
+        if not os.path.isfile(filename):
+            print_to_console("\nFile \"%s\" not found." % filename )
+        else:
+            print_to_console("\nLoading image: \"%s\"" % filename)
+            self.bbox = ghostscript.get_bbox(filename)
+            print_to_console("\nImage size: %d mm x %d mm" % (self.bbox[2], self.bbox[3]))
+
+            # Make working copy of PDF so it stays available
+            h, self.pdftmp = tempfile.mkstemp()
+            shutil.copy(filename, self.pdftmp)
+            os.close(h)
+
+            # Using temporary png file because GdkPixbuf.Pixbuf.new_from_data() appears to be broken
+            h, self.imgtmp = tempfile.mkstemp()
+            imgtmp = open(self.imgtmp, "w")
+            imgtmp.write(ghostscript.load_image(self.pdftmp, self.bbox, 144, 144, self.bbox[2]*2, self.bbox[3]*2, "pnggray").tostring())
+            imgtmp.close()
+            os.close(h)
+
+            self.img = GdkPixbuf.Pixbuf.new_from_file(self.imgtmp)
+            print self.img.get_height()
+            print self.img.get_width()
+            Gtk.Widget.queue_draw(builder.get_object("drawingareaPreview"))
+
+    def draw(self, cr):
+        Gdk.cairo_set_source_pixbuf(cr, self.img, 20, 20)
+        cr.paint()
 
 def draw_ruler(da, cr, xpos, ypos, len, scale, offset=0, vertical=False, skipzero=False):
     # Parameters
@@ -121,6 +171,7 @@ def draw_ruler(da, cr, xpos, ypos, len, scale, offset=0, vertical=False, skipzer
 def print_to_console(message):
     console.get_buffer().insert(console.get_buffer().get_end_iter(), message)
     console.scroll_mark_onscreen(console.get_buffer().get_insert())
+    print message
 
 # Load GUI layout
 builder = Gtk.Builder()
